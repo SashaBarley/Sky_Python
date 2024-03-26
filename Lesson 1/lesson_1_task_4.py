fb954c8caf7307258b781b617370c9b9ba32a78f
Sasha Barley <138057560+SashaBarley@users.noreply.github.com>
2024-03-26 12:45:04 +0700

    Add files via upload

---
+++ Lesson 1/lesson_1_task_4.py
@@ -0,0 +1,3 @@
+first_name = input("Your name: ")
+last_name = input("Your last name: ")
+print("Your name is: " + first_name +" "+ last_name)
---
+++ Lesson 1/lesson_1_task_8.py
@@ -0,0 +1,16 @@
+def my_num(num):
+    print(num, end='')
+
+#88005553535
+
+my_num(8)
+my_num(8)
+my_num(0)
+my_num(0)
+my_num(5)
+my_num(5)
+my_num(5)
+my_num(3)
+my_num(5)
+my_num(3)
+my_num(5)
