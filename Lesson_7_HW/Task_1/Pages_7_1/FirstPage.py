7c1bf6feb67e51753a29eea07a56c7f0b713e154
Sasha Barley <138057560+SashaBarley@users.noreply.github.com>
2024-06-03 19:09:22 +0700

    Update FirstPage.py

--- Lesson_7_HW/Task_1/Pages_7_1/FirstPage.py
+++ Lesson_7_HW/Task_1/Pages_7_1/FirstPage.py
@@ -1,18 +1,11 @@
 from selenium.webdriver.common.by import By
 
-class FirstPage:
-
-    url = 'https://bonigarcia.dev/selenium-webdriver-java/data-types.html'
+class SecondPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.driver.get(self.url)
-        self.driver.implicitly_wait(4)
-        self.driver.maximize_window()
-    
-    def enter_form(self, data):
-        for selector, value in data.items():
-            self.driver.find_element(By.CSS_SELECTOR, selector).send_keys(value)
 
-    def click_form(self):
-        self.driver.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
+    def check_color_red(self):
+        zip_code_red = self.driver.find_element(By.CSS_SELECTOR, "#zip-code")
+        class_attribute = zip_code_red.get_attribute("class")
+        return "alert py-2 alert-danger" in class_attribute
